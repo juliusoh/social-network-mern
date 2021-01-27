ae32881e0a4ba2ab7c243ae7ea5638feb5011778
julius oh <juliusoh@gmail.com>
2021-01-26 22:14:14 -0800

    build

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,7 +8,7 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
     <li>
-      <Link to="/profiles">Developer</Link>
+      <Link to="/profiles">Developers</Link>
     </li>
     <li>
       <Link to="/posts">Posts</Link>
